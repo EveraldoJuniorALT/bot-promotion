6ed5db06f629442215b001e7c79b7bcb6c089d5b
Everaldo Junior <everaldoizijunior@outlook.com>
2025-09-17 16:28:31 -0300

    first commit

---
+++ src/main/java/bot/promotion/JuniorAutomationApplication.java
@@ -0,0 +1,13 @@
+package bot.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bot/promotion/JuniorAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
