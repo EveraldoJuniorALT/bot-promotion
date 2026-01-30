ad64edccd266d3e89ad4827c9a0e883d379ab281
Everaldo Junior <everaldoizijunior@outlook.com>
2026-01-30 19:39:37 -0300

    Feat: Implemented the NotificationService class to handle exceptions and send notification to the Telegram group.

---
+++ src/main/java/bot/promotion/service/NotificationService.java
@@ -0,0 +1,32 @@
+package bot.promotion.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class NotificationService {
+    private final TelegramReceiveAndPost telegramReceiveAndPost;
+
+    @Autowired
+    public NotificationService(@Lazy TelegramReceiveAndPost telegramReceiveAndPost) {
+        this.telegramReceiveAndPost = telegramReceiveAndPost;
+    }
+
+    @Async
+    public void sendWarningMessage(String contextMessage) {
+        log.warn(contextMessage);
+        telegramReceiveAndPost.sendTextLogMessage("⚠️ AVISO: " + contextMessage);
+    }
+
+    @Async
+    public void sendErrorMessage(String contextMessage, Exception e) {
+        log.error("CRITICAL ERROR [{}]: {}", contextMessage, e.getMessage(), e);
+        String formatedMessage = String.format("🚨 *ERRO CRITICO*\n\n*Contexto:* %s\n*Erro:* %s",
+                contextMessage, e.getMessage());
+        telegramReceiveAndPost.sendTextLogMessage(formatedMessage);
+    }
+}
