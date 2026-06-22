ce70c32ba4fcb17b84a653a84457de50607e8705
Everaldo Junior <@everaldo.santos>
2026-06-21 23:16:49 -0300

    Feat: Implements Async Configuration

---
+++ src/main/java/bot/promotion/core/config/AsyncConfig.java
@@ -0,0 +1,47 @@
+package bot.promotion.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+
+@Component
+@EnableAsync
+@EnableScheduling
+public class AsyncConfig {
+
+    @Bean(name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("Scheduler-Thread-");
+        scheduler.initialize();
+        return scheduler;
+    }
+
+    @Bean(name = "ApiExecutor")
+    public Executor apiExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(30);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Auto-Process-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "telegramExecutor")
+    public Executor TelegramExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(15);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("Telegram-Process-");
+        executor.initialize();
+        return executor;
+    }
+}
